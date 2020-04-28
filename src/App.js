ad660f3a4ae28460e84b92f55416eed03c20c748
= <=>
2020-04-28 20:35:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+
+import logo from './logo.svg';
+import './App.css';
+import CovidGoogleMap from "./components/CovidGoogleMap";
+import PatientInfo from "./components/PatientInfo";
+import CovidDashboard from "./components/CovidDashboard";
+
+function App() {
+    return (
+        <CovidDashboard/>
+    );
+}
+
+export default App;
